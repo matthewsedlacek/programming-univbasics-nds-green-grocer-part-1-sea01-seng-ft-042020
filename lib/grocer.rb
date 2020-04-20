88eba3118546a7859bf70ae3b5a48fe1d33fb875
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-20 07:11:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,9 +27,9 @@ while counter < cart.length
       new_cart_item[:count] += 1 
     else
       new_cart_item = {
-        :item => cart[counter][:item]
-        :price => cart[counter][:price]
-        :clearance => cart[counter][:clearance]
+        :item => cart[counter][:item],
+        :price => cart[counter][:price],
+        :clearance => cart[counter][:clearance],
         :count => 1
       }
       new_cart << new_cart_item
