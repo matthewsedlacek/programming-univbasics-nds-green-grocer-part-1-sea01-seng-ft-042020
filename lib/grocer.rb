51531a1d33c60c2335f8add63b564eb178ad3184
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-20 07:03:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,7 +22,7 @@ def consolidate_cart(cart)
 new_cart = []
 counter = 0
 while counter < cart.length 
-  
+  new_cart_item = find_item_by_name_in_collection(cart[counter][:item])
 
     counter += 1
   end
